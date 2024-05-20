4f12c7937187bc8e693051de5987b576c7ee6976
cesar Tello <aquinocesarignacio@gmail.com>
2024-05-20 18:43:43 -0300

    Prueba de mongodbAtlas

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,12 +1,14 @@
 import { ProductManager } from "../class/ProductManager.js";
+import { modelProduct } from "../dao/models/product.model.js";
 
 const pm = new ProductManager();
 
 export const renderViewHoma = async (req, res) => {
-    const data = await pm.getProducts();
+    //const data = await pm.getProducts();
+    const data = await modelProduct.find().lean();
     res.status(200).render("home", { data: data })
 };
 
-export const renderViewRealTimeProducts=(req, res) => {
+export const renderViewRealTimeProducts = (req, res) => {
     res.status(200).render('realTimeProducts');
 };
