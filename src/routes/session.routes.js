91461d7e4e3d701d3cda77d9f65794096dec0d87
cesar Tello <aquinocesarignacio@gmail.com>
2024-09-08 00:05:16 -0300

    patch

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,14 +1,10 @@
-import e, { Router } from "express";
+import { Router } from "express";
 import passport from "passport";
 import sessionController from "../controllers/sessions.controller.js";
 import { initAuthStrategies, passportCall } from "../auth/passport.strategies.js";
 import { validateRequest } from "../middleware/validateRequest.middleware.js";
 import { loginSchema } from "../schema/auth.schema.js";
 import { handlePolice } from "../middleware/handlePolice.middleware.js";
-import sendResponse from "../utils/sendResponse.js";
-import CustomError from "../error/customError.error.js";
-import errorsDictionary from "../error/errorDictionary.error.js";
-import { userService } from "../services/index.js";
 
 const routesSession = Router();
 
@@ -18,7 +14,7 @@ routesSession.post('/login', validateRequest(loginSchema), passportCall('login')
 
 routesSession.get('/current', handlePolice(["USER", "PREMIUM", "ADMIN"]), sessionController.hadleCurrent)
 
-routesSession.get('/ghlogin', passport.authenticate('ghlogin', { scope: ['user'] }), async (req, res) => {});
+routesSession.get('/ghlogin', passport.authenticate('ghlogin', { scope: ['user'] }), async (req, res) => { });
 
 routesSession.get('/ghlogincallback', passportCall('ghlogin'), sessionController.handleLoginPassportGitHub)
 
